e1a4fdaa434aa96352092c380d5094dbb8adeb41
nacelease <54339008+nacelease@users.noreply.github.com>
2019-10-16 13:53:42 -0500

    Add files via upload

---
+++ Drinks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D.I._T.Data
+{
+    public class Drinks
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+
+        public string DNumber { get; set; }
+        public string Flavor { get; set; }
+        public string color { get; set; }
+    }
+}
---
+++ Foods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D.I._T.Data
+    
+{
+    public class Foods
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FID { get; set; }
+        public string Type { get; set; }
+        public int DID { get; set; }
+        [ForeignKey("DID")]
+        public Drinks drink { get; set; }
+    }
+}
